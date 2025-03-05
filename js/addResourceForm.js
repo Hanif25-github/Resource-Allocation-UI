947eff2fa43725ecc2585d2d7cecc4ef97f65e3b
Hanif Mohammad <70031945+Hanif25-github@users.noreply.github.com>
2025-03-05 17:42:28 +0530

    Update addResourceForm.js

--- js/addResourceForm.js
+++ js/addResourceForm.js
@@ -1,4 +1,3 @@
-// index.js
 
 document.getElementById('resource-form').addEventListener('submit', function (event) {
     event.preventDefault(); // Prevent the form from submitting the default way
